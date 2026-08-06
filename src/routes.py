ecf528c71e91d54a459039100f58485d78d1c82d
Maria Eduarda <maria.mirandadev@gmail.com>
2026-08-05 23:00:48 -0300

    feat(routes): definindo principais rotas da aplicação

---
+++ src/routes.py
@@ -0,0 +1,41 @@
+from src.agent import get_agent_chain
+from flask import render_template, request, jsonify
+from src import app
+
+# Inicializa o agente
+chain = get_agent_chain()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    # Dados recebidos do navegador
+    dados = request.get_json()
+    user_message = dados.get("message", "")
+
+    if not user_message:
+        return jsonify({"error":"Mensagem vazia"}), 400
+
+    if user_message.lower() == "sair":
+        return jsonify({"response":"Até logo! Se precisar, é só chamar :)"})
+    
+    try:
+        response = chain.invoke({"input":user_message}) # Envia pergunta para chain
+        answer = response.get("answer", str(response)) if isinstace(response, dict) else str(response) # Trata o texto da resposta em diversos casos
+        return jsonify({"response":answer})
+
+        '''Retorna um dicionário:
+             {
+                "input" : pergunta
+                "answer" : resposta
+             }
+            
+            '''
+
+    except Exception as e:
+        print(f"Algo deu errado: {e}")
+        return jsonify({"error":"Ocorreu um erro interno."}), 500
+
+        
